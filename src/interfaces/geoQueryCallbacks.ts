7c0553f99fe85c960103c2b5f21d0145854b77d5
Emre Yavuz <siemya@gmail.com>
2018-11-22 13:09:56 +0300

    Update geoQueryCallbacks.ts

--- src/interfaces/geoQueryCallbacks.ts
+++ src/interfaces/geoQueryCallbacks.ts
@@ -3,6 +3,7 @@ export type KeyCallback = (key?: string, document?: any, distanceFromCenter?: nu
 
 export interface GeoQueryCallbacks {
   ready: ReadyCallback[];
+  error: ErrorCallback[];
   key_entered: KeyCallback[];
   key_exited: KeyCallback[];
   key_moved: KeyCallback[];
